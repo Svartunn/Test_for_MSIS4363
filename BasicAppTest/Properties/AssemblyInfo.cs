a2c8c66326c2300b5383ed2f711525e666a61600
Matt McMichael <matt.mcmichael@okstate.edu>
2022-09-13 12:19:22 -0500

    Add project files.

---
+++ BasicAppTest/Properties/AssemblyInfo.cs
@@ -0,0 +1,20 @@
+using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+[assembly: AssemblyTitle("BasicAppTest")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("")]
+[assembly: AssemblyProduct("BasicAppTest")]
+[assembly: AssemblyCopyright("Copyright ©  2022")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+[assembly: ComVisible(false)]
+
+[assembly: Guid("ce71eda0-2fe6-4ea5-a7d6-3c9a8902cfdc")]
+
+// [assembly: AssemblyVersion("1.0.*")]
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
---
+++ MM Basic App GitHub/Class1.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace MM_Basic_App_GitHub
+{
+    public class Class1
+    {
+        public String Name;
+        public Class1()
+        {
+            Name = "Matt McMichael - The Code Master"; //Insert your name and moniker 
+        }
+    }
+}
